233e519c8671661ef7780f19030c43bbeba44891
aqureshiest <adeel.qureshi@earnest.com>
2020-06-08 19:11:00 -0700

    added the code to see which images arent being recognized

---
+++ another_training.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+def sigmoid(z, deriv=False):
+    if(deriv == True):
+        return z*(1-z)
+
+    s = 1 / (1 + np.exp(-z))
+    return s
+
+x = np.array([[0,0,1],
+            [0,1,1],
+            [1,0,1],
+            [1,1,1]])
+
+y = np.array([[0],
+            [1],
+            [1],
+            [0]])
+
+# define training set and labels
+train_set = np.random.rand(3, 4)
+labels = np.random.rand(4, 1)
+
+# train the model
+for i in range(10000):
+
+    # activations
+    l0 = x
+    l1 = sigmoid(np.dot(l0, train_set))
+    l2 = sigmoid(np.dot(l1, labels))
+
+    # find error
+    l2_error = y - l2
+
+    if (i%1000 == 0):
+        print("Error: " + str(np.mean(np.abs(l2_error))))
+
+    # which direction to fix the error
+    l2_delta = l2_error * sigmoid(l2, deriv=True)
+
+    # prop back to previous layer
+    l1_error = l2_delta.dot(labels.T)
+
+    # which direction for prevoius layer
+    l1_delta = l1_error * sigmoid(l1, deriv=True)
+
+    # update weights
+    train_set += l1.T.dot(l2_delta)
+    labels += l0.T.dot(l1_delta)
+
+
+
+
--- one_layer_nn/recognize_digits_keras.py
+++ one_layer_nn/recognize_digits_keras.py
@@ -1,11 +1,24 @@
 import pandas as pd
+import numpy as np
 from keras.layers import Dense
 from keras.models import Sequential
 from keras.optimizers import RMSprop, Adadelta, Adam
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
+import matplotlib .pyplot as plt
+
+def print_img(digits, indx):
+    plt.gray()
+    plt.matshow(dig.images[indx])
+    plt.show()
 
 dig = load_digits()
+
+# print_img(dig, 922)
+# 130 = 0
+# 1174 = 7
+# 265 = 9
+
 onehot_target = pd.get_dummies(dig.target)
 x_train, x_val, y_train, y_val = train_test_split(dig.data, onehot_target, test_size=0.1, random_state=20)
 
@@ -17,7 +30,19 @@ model.add(Dense(10, activation='softmax'))
 model.summary()
 
 model.compile(optimizer=Adadelta(), loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-model.fit(x_train, y_train, epochs=50, batch_size=64)
+model.fit(x_train, y_train, epochs=50, batch_size=64, verbose=False)
+
+scores = model.evaluate(x_train, y_train)
+print("training set %s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 scores = model.evaluate(x_val, y_val)
-print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+print("test set %s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+# preds = model.predict_classes(x_val)
+
+# indx=0
+# for index, row in y_val.iterrows():
+#     if(row.tolist()[preds[indx]] == 0):
+#         print("expected {} got {} at indx {} and index {}".format(row.tolist(), preds[indx], indx, index))
+#         print_img(dig, index)
+#     indx = indx + 1
